a029bce26a91925f9669e72fa1b9380d7c9925d6
André Luiz Girol <andregirol@gmail.com>
2016-06-10 17:51:21 -0300

    Head First - Passos 3 e 4

--- HFAjax/scripts/thumbs.js
+++ HFAjax/scripts/thumbs.js
@@ -24,9 +24,59 @@ function initPage() {
 		
 		}		
 	}
-	
-	
+
 	// DEBUG
 	console.log("O valor de thumbs é: " + thumbs);
 	console.log("To aqui mano");	
+}
+
+// Cria um HTTP Request
+function createRequest() {
+	try {
+		
+		var request = new XMLHttpRequest();
+		
+		} catch (tryMS) {
+			
+			try {
+				request = new ActiveXObject("Msxml12.XMLHTTP");
+				
+				} catch (otherMS) {
+					try {				
+					request = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (failed) {
+					request = null;
+					}
+			}
+			
+	}
+	return request;
+}
+/*
+* Envia requisição das descrições dos itens para o servidor
+* O parâmetro itemName é usado para enviar o nome da imagem para o servidor
+*/ 
+
+function getDetails(itemName) {
+	request = createRequest();
+	
+	if(request == null){
+		alert("Não deu pra criar a requisição");
+		return;	
+	}
+	var url = "getDetails.php?ImageID=" + escape(itemName);
+	request.open("GET",url,true);
+	// aqui vai uma callback
+	request.onreadystatechange = displayDetails;
+	request.send(null);
+	 
+}
+
+function displayDetails() {
+	if (request.readyState == 4) {
+		if(request.status == 200) {
+		var detailDiv = document.getElementById("description");
+		detailDiv.innerHTML = request.responseText;		
+		}	
+	}
 }
