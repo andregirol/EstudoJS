66f3dddbcd20d2c329097953c3237410b86e5b03
André Luiz Girol <andregirol@gmail.com>
2017-04-26 14:32:09 -0300

    Aprendendo 'switch' em JavaScript

---
+++ Bonieky/switch/aula.js
@@ -0,0 +1,16 @@
+var num = prompt("Insira um número:");
+
+num = parseInt(num);
+
+switch(num){
+	case 0:
+		alert("o número é 0");
+		break;
+
+	case 1:
+		alert("o número é 1");
+		break;
+
+	default:
+		alert("Não é zero nem um!");
+}
