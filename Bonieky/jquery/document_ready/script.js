83795bc26ba81b3286d6b35df0da0861333f9e04
André Luiz Girol <andregirol@gmail.com>
2017-04-28 17:05:21 -0300

    Arquivos para testes da biblioteca jQuery

---
+++ Bonieky/jquery/document_ready/script.js
@@ -0,0 +1,11 @@
+// Seu código aqui...
+$(document).ready(function(){
+	//alert("olá você!");
+
+	$('a').attr('href','http://www.google.com');
+
+});
+
+$(document).ready(function(){
+	console.log("Rodando jQuery 3.2.1");
+});
